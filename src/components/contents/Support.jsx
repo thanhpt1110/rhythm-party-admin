a76222358424c9a402db10483e195db859909fda
Tuan-Thanh <thanhphantuan1110@gmail.com>
2023-12-10 23:50:25 +0700

    Add UI for Support

--- src/components/contents/Support.jsx
+++ src/components/contents/Support.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Support = () => {
   return (
-    <div>Support</div>
+    <div className='flex flex-col py-4'>
+        <div className='mb-4 w-full'>
+            <div className='text-lg font-bold mb-2'>
+                Title
+            </div>
+            <div>
+                <input type="text" name="Title" id="title" className='w-full p-2 border-2 border-gray-300 rounded' />
+            </div>
+        </div>
+        <div className='flex-1 mb-4 w-full'>
+            <div className='text-lg font-bold mb-2'>
+                Content
+            </div>
+            <div>
+                <textarea className='w-full h-full p-2 border-2 border-gray-300 rounded'></textarea>
+            </div>
+        </div>
+        <div>
+            <input type="submit" value="Submit" className=' w-40 bg-blue-600 hover:cursor-pointer hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out' />
+        </div>
+    </div>
   )
 }
 
-export default Support
+export default Support;
