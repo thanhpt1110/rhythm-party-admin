50f2b3b49023d2c9a312140790f87e7f6cce0ba0
Tuan-Thanh <thanhphantuan1110@gmail.com>
2023-12-11 21:44:00 +0700

    Fix UI for Support

--- src/components/contents/Support.jsx
+++ src/components/contents/Support.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 
 const Support = () => {
   return (
-    <div className='flex flex-col py-4'>
+    <div className='flex flex-col py-4 h-full'>
         <div className='mb-4 w-full'>
-            <div className='text-lg font-bold mb-2'>
+            <div className='text-xl font-bold mb-2'>
                 Title
             </div>
             <div>
-                <input type="text" name="Title" id="title" className='w-full p-2 border-2 border-gray-300 rounded' />
+                <input type="text" name="Title" id="title" className='w-full p-2 border-2 border-gray-300 rounded focus:border-violet-700' />
             </div>
         </div>
-        <div className='flex-1 mb-4 w-full'>
-            <div className='text-lg font-bold mb-2'>
+        <div className='flex flex-col mb-4 w-full h-full'>
+            <div className='text-xl font-bold mb-2'>
                 Content
             </div>
-            <div>
-                <textarea className='w-full h-full p-2 border-2 border-gray-300 rounded'></textarea>
+            <div className='h-full '>
+                <textarea className='w-full h-full p-2 border-2 border-gray-300 rounded resize-none focus:border-violet-700'></textarea>
             </div>
         </div>
         <div>
-            <input type="submit" value="Submit" className=' w-40 bg-blue-600 hover:cursor-pointer hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out' />
+            <input type="submit" value="Submit" className='px-7 py-3 bg-violet-800 hover:cursor-pointer hover:bg-violet-700 text-white font-bold rounded-lg transition duration-300 ease-in-out' />
         </div>
     </div>
   )
