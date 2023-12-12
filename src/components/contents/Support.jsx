e6b6b5fc63df76117c4a2e2d4f763bc5fdd65a74
Tuan-Thanh <thanhphantuan1110@gmail.com>
2023-12-12 09:11:56 +0700

    Update handleSubmit Support

--- src/components/contents/Support.jsx
+++ src/components/contents/Support.jsx
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Support = () => {
-  return (
-    <div className='flex flex-col py-4 h-full'>
-        <div className='mb-4 w-full'>
-            <div className='text-xl font-bold mb-2'>
-                Title
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleSubmit = () => {
+        // Xử lý dữ liệu title và content ở đây
+        console.log(title, content);
+    }
+
+    return (
+        <div className='flex flex-col py-4 h-full'>
+            <div className='mb-4 w-full'>
+                <div className='text-xl font-bold mb-2'>
+                    Title
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        name="Title"
+                        id="title"
+                        className='w-full p-2 border-2 border-gray-300 rounded focus:border-violet-700'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className='flex flex-col mb-4 w-full h-full'>
+                <div className='text-xl font-bold mb-2'>
+                    Content
+                </div>
+                <div className='h-full '>
+                    <textarea
+                        className='w-full h-full p-2 border-2 border-gray-300 rounded resize-none focus:border-violet-700'
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    ></textarea>
+                </div>
             </div>
             <div>
-                <input type="text" name="Title" id="title" className='w-full p-2 border-2 border-gray-300 rounded focus:border-violet-700' />
+                <input
+                    onClick={handleSubmit}
+                    type="submit"
+                    value="Submit"
+                    className='px-7 py-3 bg-violet-800 hover:cursor-pointer hover:bg-violet-700 text-white font-bold rounded-lg transition duration-300 ease-in-out'
+                />
             </div>
         </div>
-        <div className='flex flex-col mb-4 w-full h-full'>
-            <div className='text-xl font-bold mb-2'>
-                Content
-            </div>
-            <div className='h-full '>
-                <textarea className='w-full h-full p-2 border-2 border-gray-300 rounded resize-none focus:border-violet-700'></textarea>
-            </div>
-        </div>
-        <div>
-            <input type="submit" value="Submit" className='px-7 py-3 bg-violet-800 hover:cursor-pointer hover:bg-violet-700 text-white font-bold rounded-lg transition duration-300 ease-in-out' />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Support;
